a683e558298e9e16f5858cc8be852466cd6b6e8f
pratham-cogo <pratham.kumar@cogoport.com>
2025-02-27 12:13:11 +0530

    user

--- src/services/users/apis/register.py
+++ src/services/users/apis/register.py
@@ -6,5 +6,9 @@ from services.users.params import CreateUser
 def register_user(request: CreateUser) -> User:
     hashed_password = hash_password(request.password)
     with db.atomic():
+        user: User = User.get_or_none(User.username == request.username)
+        if user:
+            return "Username not available"
+
         user: User = User.create(username=request.username, password_hash=hashed_password)
     return {"id": user.id, "username": user.username}
